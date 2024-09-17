976cf19428219c18afa208f7153dc59f51917980
Dan Adrian Mirea <damirea84@gmail.com>
2024-09-17 19:08:03 +0300

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# elements-of-matrix" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/elements-of-matrix.git
-git push -u origin main
